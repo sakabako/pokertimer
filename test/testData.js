3f20494e8eb0b72cd305211296baf70405d2de45
Stu Kabakoff <sakabako@gmail.com>
2012-09-30 17:06:17 -0400

    made first level of test data end after 3 seconds

--- test/testData.js
+++ test/testData.js
@@ -3,7 +3,7 @@ define(function( require, exports, module ) {
 		return {
 			id: 'test',
 			state: [
-				{blinds:'100/200', game:'Hold Em', duration:600000, time:300000},
+				{blinds:'100/200', game:'Hold Em', duration:600000, time:3000},
 				{blinds:'200/400', game:'Omaha', duration:600000},
 				{blinds:'400/800', game:'Hold Em', duration:600000}
 			],
